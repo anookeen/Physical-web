d9f0159b87b1365139510105fedbb3257c2fd5db
Anoosha keen <anookeen@gmail.com>
2017-03-12 22:56:55 +0500

    Running Median

---
+++ Median.cpp
@@ -0,0 +1,34 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+	int n=0;int *w=new int[n];int temp=0;double t=0;
+	int *r=new int[n];
+	cin>>n;
+	for(int i=0;i<n;i++){
+	cin>>w[i];
+if(i>=1){
+	for(int k=0;k<i;k++){
+	for(int j=1;j<=i;j++){
+	if(w[j-1]>w[j]){
+	temp=w[j-1];
+	w[j-1]=w[j];
+	w[j]=temp;
+	}}}}
+
+	}*/
+
+	
+	/*for(int i=0;i<n;i++){
+	cout<<w[i]<<endl;
+	}*/
+	/*int l=0;
+	for(int u=0;u<n;u++){
+		if(u==0){t+=w[u];cout<<w[u]<<endl;}
+		if(u!=0){
+		t+=(double)(w[u]);
+		cout<<t/(u+1)<<endl;}
+		}
+	
+
+	system("pause");
+}*/
